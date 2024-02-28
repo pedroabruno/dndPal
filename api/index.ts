14a77d7263e5d9e85772dd5b184596bb812c2751
pedroabruno <pedroabruno@gmail.com>
2024-02-28 19:33:45 -0300

    disable cors 2

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,30 @@ const express = require('express');
 const app = express();
 // const monstersResources = require('./resources/monstersResources.ts')
 const monsters = require('../dbData/monsters.json')
+var cors = require('cors');
+
+
+var allowedOrigins = ['http://localhost:3000','https://dnd-pal-page.vercel.app/'];
+
+app.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
+
+app.use((req,res,next)=>{
+    // use for checking credentials
+    next()
+})
+
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/monsters", (req, res) => {
