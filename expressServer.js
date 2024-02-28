050bdf51560ee81ad4a92be24d56018f1327b445
Pedro Bruno <pedroabruno@gmail.com>
2024-02-28 12:51:51 -0300

    Add files via upload

---
+++ expressServer.js
@@ -0,0 +1,22 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const monsters = require("./monsters.json");
+
+const express = require('express')
+const sv = express()
+
+const PORT = process.env.PORT ?? 1234
+
+sv.use((req,res,next)=>{
+    //check permisos
+    console.log('I should be checking permissions')
+    next()
+})
+
+sv.get('/monsters', (req,res)=> {
+    res.json(monsters[0])
+})
+
+sv.listen(PORT, () => {
+    console.log(`sv running on port: ${PORT}`)
+})
